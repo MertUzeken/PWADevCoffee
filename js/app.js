b88f13ecff160bcad03e2e20aa9cd4437215d3ad
MertUzeken <66569850+MertUzeken@users.noreply.github.com>
2023-11-02 17:29:34 +0100

    LLLL2

--- js/app.js
+++ js/app.js
@@ -60,20 +60,6 @@ function takePhoto() {
 }
 
 
-const showCoffees = () => {
-  let output = "";
-  coffees.forEach(
-    ({ name, image }) =>
-      (output += `
-              <div class="card">
-                <img class="card--avatar" src=${image} />
-                <h1 class="card--title">${name}</h1>
-                <a class="card--link" href="#">Taste</a>
-              </div>
-              `)
-  );
-  container.innerHTML = output;
-};
 
 document.addEventListener("DOMContentLoaded", showCoffees);
 
