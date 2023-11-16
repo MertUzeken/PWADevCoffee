2c0607fbc393669af54bf9dc04f1e7b36941e7ab
MertUzeken <66569850+MertUzeken@users.noreply.github.com>
2023-11-16 16:18:33 +0100

    fixed

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ if ('storage' in navigator && 'estimate' in navigator.storage) {
     .then(estimate => {
       document.getElementById('usage').innerHTML = estimate.usage;
       document.getElementById('quota').innerHTML = estimate.quota;
-      document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota);
+      document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota).toFixed(6);
     });
 }
 
