d4e8f787ae00ea5cf316f4b29c226be50383bc95
MertUzeken <66569850+MertUzeken@users.noreply.github.com>
2023-11-30 15:05:13 +0100

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,131 +1,14 @@
 const container = document.querySelector(".container");
 
 
-if ('storage' in navigator && 'estimate' in navigator.storage) {
-  navigator.storage.estimate()
-    .then(estimate => {
-      document.getElementById('usage').innerHTML = estimate.usage;
-      document.getElementById('quota').innerHTML = estimate.quota;
-      document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota).toFixed(6);
-    });
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
 }
 
-if ('storage' in navigator && 'persisted' in navigator.storage) {
-  navigator.storage.persisted()
-    .then(persisted => {
-      document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
-    });
-}
-
-function requestPersistence() {
-  if ('storage' in navigator && 'persist' in navigator.storage) {
-    navigator.storage.persist()
-      .then(persisted => {
-        document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
-      });
-  }
-}
-
-//_____________________________ END OF QUOTA _________________________________________________
-//_____________________________ Start OF Offline Storage _____________________________________
-
-if ('localStorage' in window || 'sessionStorage' in window) {
-  var selectedEngine;
-
-  var logTarget = document.getElementById('target');
-  var valueInput = document.getElementById('value');
-
-  var reloadInputValue = function () {
-  console.log(selectedEngine, window[selectedEngine].getItem('myKey'))
-    valueInput.value = window[selectedEngine].getItem('myKey') || '';
-  }
-  
-  var selectEngine = function (engine) {
-    selectedEngine = engine;
-    reloadInputValue();
-  };
-
-  function handleChange(change) {
-    var timeBadge = new Date().toTimeString().split(' ')[0];
-    var newState = document.createElement('p');
-    newState.innerHTML = '' + timeBadge + ' ' + change + '.';
-    logTarget.appendChild(newState);
-  }
-
-  var radios = document.querySelectorAll('#selectEngine input');
-  for (var i = 0; i < radios.length; ++i) {
-    radios[i].addEventListener('change', function () {
-      selectEngine(this.value)
-    });
-  }
-  
-  selectEngine('localStorage');
-
-  valueInput.addEventListener('keyup', function () {
-    window[selectedEngine].setItem('myKey', this.value);
-  });
-
-  var onStorageChanged = function (change) {
-    var engine = change.storageArea === window.localStorage ? 'localStorage' : 'sessionStorage';
-    handleChange('External change in ' + engine + ': key ' + change.key + ' changed from ' + change.oldValue + ' to ' + change.newValue + '');
-    if (engine === selectedEngine) {
-      reloadInputValue();
-    }
-  }
-
-  window.addEventListener('storage', onStorageChanged);
-}
-
-
-//_____________________________ END OF Offline Storage_________________________________________________
-//_____________________________ Start OF getFile _____________________________________
-function getReadFile(reader, i) {
-  return function () {
-    var li = document.querySelector('[data-idx="' + i + '"]');
-
-    li.innerHTML += 'File starts with "' + reader.result.substr(0, 25) + '"';
-  }
-}
-
-function readFiles(files) {
-  document.getElementById('count').innerHTML = files.length;
-
-  var target = document.getElementById('target');
-  target.innerHTML = '';
-
-  for (var i = 0; i < files.length; ++i) {
-    var item = document.createElement('li');
-    item.setAttribute('data-idx', i);
-    var file = files[i];
-
-    var reader = new FileReader();
-    reader.addEventListener('load', getReadFile(reader, i));
-    reader.readAsText(file);
-
-    item.innerHTML = '' + file.name + ', ' + file.type + ', ' + file.size + ' bytes, last modified ' + file.lastModifiedDate + '';
-    target.appendChild(item);
-  };
-}
-
-async function writeFile() {
-  if (!window.chooseFileSystemEntries) {
-    alert('Native File System API not supported');
-    return;
-  }
-  
-  const target = document.getElementById('target2');
-  target.innerHTML = 'Opening file handle...';
-  
-  const handle = await window.chooseFileSystemEntries({
-    type: 'save-file',
-  });
-  
-  const file = await handle.getFile()
-  const writer = await handle.createWriter();
-  await writer.write(0, 'Hello world from What Web Can Do!');
-  await writer.close()
-  
-  target.innerHTML = 'Test content written to ' + file.name + '.';
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
 
 
