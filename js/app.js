65ef2c8881fd7ac726bdc7d38875ab902f4a8746
MertUzeken <66569850+MertUzeken@users.noreply.github.com>
2023-11-30 16:05:03 +0100

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,65 +1,8 @@
 const container = document.querySelector(".container");
 
+let transactions = [];
+let totalBalance = 0;
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-
-
-
-
-
-
-if ('getBattery' in navigator || ('battery' in navigator && 'Promise' in window)) {
-  var target = document.getElementById('target');
-
-  function handleChange(change) {
-    var timeBadge = new Date().toTimeString().split(' ')[0];
-    var newState = document.createElement('p');
-    newState.innerHTML = '' + timeBadge + ' ' + change + '.';
-    target.appendChild(newState);
-  }
-  
-  function onChargingChange() {
-    handleChange('Battery charging changed to ' + (this.charging ? 'charging' : 'discharging') + '')
-  }
-  function onChargingTimeChange() {
-    handleChange('Battery charging time changed to ' + this.chargingTime + ' s');
-  }
-  function onDischargingTimeChange() {
-    handleChange('Battery discharging time changed to ' + this.dischargingTime + ' s');
-  }
-  function onLevelChange() {
-    handleChange('Battery level changed to ' + this.level + '');
-  }
-
-  var batteryPromise;
-  
-  if ('getBattery' in navigator) {
-    batteryPromise = navigator.getBattery();
-  } else {
-    batteryPromise = Promise.resolve(navigator.battery);
-  }
-  
-  batteryPromise.then(function (battery) {
-    document.getElementById('charging').innerHTML = battery.charging ? 'charging' : 'discharging';
-    document.getElementById('chargingTime').innerHTML = convertSecondsToMinutes(battery.chargingTime) + 'min';
-    document.getElementById('dischargingTime').innerHTML = convertSecondsToMinutes(battery.dischargingTime) + 'min';
-    document.getElementById('level').innerHTML = battery.level;
-    
-    battery.addEventListener('chargingchange', onChargingChange);
-    battery.addEventListener('chargingtimechange', onChargingTimeChange);
-    battery.addEventListener('dischargingtimechange', onDischargingTimeChange);
-    battery.addEventListener('levelchange', onLevelChange);
-  });
-  function convertSecondsToMinutes(seconds) {
-    return Math.floor(seconds / 60);
-}
-}
 
 
 
