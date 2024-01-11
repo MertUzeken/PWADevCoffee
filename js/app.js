7cfb5cb85063c7348704987fc7237ced6e9b0fff
MertUzeken <66569850+MertUzeken@users.noreply.github.com>
2024-01-11 18:45:36 +0100

    revert code to oldstate

--- js/app.js
+++ js/app.js
@@ -34,38 +34,48 @@ var elements = document.querySelectorAll('.test-element');
  document.getElementById("addBoxBtn").addEventListener("click", addBox);
 
 function startDrag(e) {
-
-  var pos = [this.offsetLeft, this.offsetTop];
-  var origin = getCoors(e);
-
   this.ontouchmove = this.onmspointermove = moveDrag;
+
   this.ontouchend = this.onmspointerup = function () {
     this.ontouchmove = this.onmspointermove = null;
     this.ontouchend = this.onmspointerup = null;
   }
 
+  var pos = [this.offsetLeft, this.offsetTop];
   var that = this;
+  var origin = getCoors(e);
 
   function moveDrag(e) {
     var currentPos = getCoors(e);
     var deltaX = currentPos[0] - origin[0];
     var deltaY = currentPos[1] - origin[1];
+    this.style.left = (pos[0] + deltaX) + 'px';
+    this.style.top = (pos[1] + deltaY) + 'px';
+    return false; // cancels scrolling
+  }
 
-   
-    that.style.left = (pos[0] + deltaX) + 'px';
-    that.style.top = (pos[1] + deltaY) + 'px';
-    
-    return false;
+  function getCoors(e) {
+    var coors = [];
+      if (e.targetTouches && e.targetTouches.length) {
+        var thisTouch = e.targetTouches[0];
+        coors[0] = thisTouch.clientX;
+        coors[1] = thisTouch.clientY;
+      } else {
+        coors[0] = e.clientX;
+        coors[1] = e.clientY;
+      }
+    return coors;
   }
 }
 
-
+  var elements = document.querySelectorAll('.test-element');
   [].forEach.call(elements, function (element) {
-    element.ontouchstart = element.onmspointerdown = startDrag;
-    });
+  element.ontouchstart = element.onmspointerdown = startDrag;
+  });
 
-    document.ongesturechange = function () {
-                              return false;
+
+  document.ongesturechange = function () {
+    return false;
   }
 
 
