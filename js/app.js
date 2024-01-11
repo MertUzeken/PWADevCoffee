f79cba5c2d8939fea6fce0b1e652dd699080b7a8
MertUzeken <66569850+MertUzeken@users.noreply.github.com>
2024-01-11 17:34:21 +0100

    fix 1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 const container = document.querySelector(".container");
-var pos = [this.offsetLeft, this.offsetTop];
-var that = this;
-var origin = getCoors(e);
+
+  function getCoors(e) {
+    var coors = [];
+    if (e.targetTouches && e.targetTouches.length) {
+    var thisTouch = e.targetTouches[0];
+    coors[0] = thisTouch.clientX;
+    coors[1] = thisTouch.clientY;
+    } else {
+        coors[0] = e.clientX;
+        coors[1] = e.clientY;
+    }
+    return coors;
+  }
 
 var elements = document.querySelectorAll('.test-element');
 
@@ -23,37 +33,31 @@ var elements = document.querySelectorAll('.test-element');
  // Add new box on button click
  document.getElementById("addBoxBtn").addEventListener("click", addBox);
 
+function startDrag(e) {
+  var pos = [this.offsetLeft, this.offsetTop];
+  var origin = getCoors(e);
 
-  function startDrag(e) {
-    this.ontouchmove = this.onmspointermove = moveDrag;
+  this.ontouchmove = this.onmspointermove = moveDrag;
 
-    this.ontouchend = this.onmspointerup = function () {
-    this.ontouchmove = this.onmspointermove = null;
-    this.ontouchend = this.onmspointerup = null;
+  this.ontouchend = this.onmspointerup = function () {
+  this.ontouchmove = this.onmspointermove = null;
+  this.ontouchend = this.onmspointerup = null;
   }
 
+  var that = this;
 
   function moveDrag(e) {
     var currentPos = getCoors(e);
     var deltaX = currentPos[0] - origin[0];
     var deltaY = currentPos[1] - origin[1];
-    this.style.left = (pos[0] + deltaX) + 'px';
-    this.style.top = (pos[1] + deltaY) + 'px';
+    that.style.left = (pos[0] + deltaX) + 'px';
+    that.style.top = (pos[1] + deltaY) + 'px';
     return false; // cancels scrolling
   }
+}
 
-  function getCoors(e) {
-    var coors = [];
-    if (e.targetTouches && e.targetTouches.length) {
-    var thisTouch = e.targetTouches[0];
-    coors[0] = thisTouch.clientX;
-    coors[1] = thisTouch.clientY;
-    } else {
-        coors[0] = e.clientX;
-        coors[1] = e.clientY;
-    }
-    return coors;
-  }
+
+
 
   }
 
